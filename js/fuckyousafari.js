b03456e441400c3c950c18a4f366d0ec62d4100b
dift12 <diftbanda@gmail.com>
2024-07-06 22:56:57 -0300

    Make final fix for safari

--- js/fuckyousafari.js
+++ js/fuckyousafari.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     let isSafariMobile = /safari/.test(ua) && /iphone|ipod|ipad/.test(ua) && !/chrome/.test(ua);
   
     if (isSafariMobile) {
+      alert("Parece que estas intentando acceder a este sitio desde Apple IOs, una version simplificada del sitio, compatible con tu navegador se cargara en tu dispositivo.")
       document.body.classList.add('safari-mobile');
     }
-    else{
-        document.body.classList.add('pene');
-    }
+   
+
   });
