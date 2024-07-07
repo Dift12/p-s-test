03e1d92efc5301aa077d84d5660fc08ca9e83dff
dift12 <diftbanda@gmail.com>
2024-07-06 23:20:34 -0300

    Add safari compatibility mode

--- js/fuckyousafari.js
+++ js/fuckyousafari.js
@@ -2,10 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     let ua = navigator.userAgent.toLowerCase();
     let isSafariMobile = /safari/.test(ua) && /iphone|ipod|ipad/.test(ua) && !/chrome/.test(ua);
   
+    let fart=new Audio("../sounds/fart.mp3")
+
     if (isSafariMobile) {
       alert("Parece que estas intentando acceder a este sitio desde Apple IOs, una version simplificada del sitio, compatible con tu navegador se cargara en tu dispositivo.")
-      document.body.classList.add('safari-mobile');
+      //document.body.classList.add('safari-mobile');
+      document.head.innerHTML="";
+      fart.play()
+      alert("Disfrute de la experience Safari")
     }
-   
 
   });
