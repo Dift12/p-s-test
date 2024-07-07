7fa2e58659615a5d99e6ba1e4d3161050a3219b0
dift12 <diftbanda@gmail.com>
2024-07-06 23:44:48 -0300

    Make autoplay policy work around

--- js/fuckyousafari.js
+++ js/fuckyousafari.js
@@ -2,14 +2,23 @@ document.addEventListener("DOMContentLoaded", function() {
     let ua = navigator.userAgent.toLowerCase();
     let isSafariMobile = /safari/.test(ua) && /iphone|ipod|ipad/.test(ua) && !/chrome/.test(ua);
   
-    let fart=new Audio("../sounds/fart.mp3")
+    
 
     if (isSafariMobile) {
-      alert("Parece que estas intentando acceder a este sitio desde Apple IOs, una version simplificada del sitio, compatible con tu navegador se cargara en tu dispositivo.")
+
+      let fart=new Audio("../sounds/fart.mp3");
+
+      /*Opefully this gets around the autoplay policy, and the user will interact with the site first*/
+      setTimeout(()=>{
+        alert("Parece que estas navegando en este sitio desde Apple IOs, una version simplificada del sitio, compatible con tu navegador se cargara en tu dispositivo.")
+        fart.play();
+        const links=document.head.querySelectorAll("link");
+        links.forEach(link=> link.href="")
+        alert("Disfrute de la experience Safari")
+      }, 5000)
+      
       //document.body.classList.add('safari-mobile');
-      document.head.innerHTML="";
-      fart.play()
-      alert("Disfrute de la experience Safari")
-    }
+
+  }
 
   });
